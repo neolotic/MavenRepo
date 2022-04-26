23d224034fb4f276e503a614e1155e8b079dd48a
neolotic <satej.kamble92@gmail.com>
2022-04-26 10:57:50 +0530

    Committed Maven code

---
+++ src/test/java/firstmaven/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package firstmaven;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		System.out.println("...Hello World");
+	}
+	
+	
+}
